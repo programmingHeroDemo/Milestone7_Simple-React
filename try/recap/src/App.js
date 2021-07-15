64d06a4eb7ec29c5191dfe9f4052d02cfdbca601
MonadWizard <monad.wizard.r@gmail.com>
2021-07-15 22:25:50 +0600

    recap useState

--- try/recap/src/App.js
+++ try/recap/src/App.js
@@ -1,14 +1,54 @@
+import { useState } from 'react'
+
 function App() {
+    const man = [
+        { name: 'biskut pagla', age: 1000 },
+        { name: 'halum', age: 15 },
+        { name: 'tuktuki', age: 11 },
+        { name: 'akri', age: 10 },
+        { name: 'shiku', age: 12 },
+    ]
+
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello Rakib..!</h1>
-                <Nayok name="rakib" age="28"></Nayok>
-                <Nayok></Nayok>
+                <h1>Hello Akfa..!</h1>
             </header>
+
+            <Nayok name={man[0].name} age={man[0].age}></Nayok>
+
+            <MovieCount></MovieCount>
+            {man.map((m) => (
+                <Nayok name={m.name} age={m.age}></Nayok>
+            ))}
         </div>
     )
 }
+
+const MovieCount = () => {
+    const countStyle = {
+        textAlign: 'right',
+        margin: '20px',
+        border: '2px solid green',
+        borderRadius: 20,
+        padding: '10px',
+    }
+
+    const [count, setcount] = useState(0)
+
+    const click = () => setcount(count + 1)
+
+    return (
+        <div style={countStyle}>
+            <button onClick={click}>add movie</button>
+
+            <MovieDisplay number={count}></MovieDisplay>
+            <MovieDisplay number={count + 10}></MovieDisplay>
+        </div>
+    )
+}
+
+const MovieDisplay = (props) => <h4>Movies I have accted : {props.number}</h4>
 
 const Nayok = (props) => {
     const nayokStyle = {
@@ -20,9 +60,9 @@ const Nayok = (props) => {
 
     return (
         <div style={nayokStyle}>
-            <h1>Ami nayok "{props.name}" noi , ami director</h1>
+            <h1>Ami "{props.name}" </h1>
             <p style={{ color: 'green', fontWeight: 'bold' }}>
-                I have done 5 movies in age: {props.age}
+                amar boyosh: {props.age}
             </p>
         </div>
     )
